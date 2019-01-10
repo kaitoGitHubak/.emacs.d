585ce6de8c09c601ca5c98f1200d7bebfe92f45b
kato <kaito0406goak@gmail.com>
2019-01-10 20:35:18 +0900

    ファイルの追加

--- init.el
+++ init.el
@@ -39,3 +39,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ricty Diminished" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
+

094109d3e2f020058c0d2b45cd19084110abcb23
kato <kaito0406goak@gmail.com>
2019-02-14 13:51:08 +0900

    環境移行用変更

--- inits/04_key-bindings.el
+++ inits/04_key-bindings.el
@@ -1,3 +1,5 @@
 (keyboard-translate ?\C-h ?\C-?)
 ;; C-kで行全体を削除する
 (setq kill-whole-line t)
+
+(normal-erase-is-backspace-mode 0)
--- inits/10_mozc.el
+++ inits/10_mozc.el
@@ -3,4 +3,6 @@
 (set-language-environment "Japanese")
 (setq default-input-method "japanese-mozc")
 (setq mozc-candidate-style 'overlay)
+;;キーバインド変更をする
+;;ctrl+oだと反応しない
 (global-set-key (kbd "C-o") 'toggle-input-method)
